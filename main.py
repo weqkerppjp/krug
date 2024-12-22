049b7f194ee7701fd27fd50b294e685e1cc08d18
Just <legomen72@mail.ru>
2024-12-22 14:22:31 +0500

    done

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import sys
 import random
-from PyQt6 import QtWidgets, uic
+from PyQt6 import QtWidgets
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6.QtCore import Qt
 
 
 class CircleApp(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setWindowTitle("Random Circles")
+        self.setGeometry(100, 100, 800, 600)
+
         self.circles = []
+        self.pushButton = QtWidgets.QPushButton("Добавить окружность", self)
+        self.pushButton.setGeometry(10, 10, 200, 30)
         self.pushButton.clicked.connect(self.add_circle)
 
     def add_circle(self):
         diameter = random.randint(10, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.circles.append((x, y, diameter, color))
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.setBrush(QColor('yellow'))
-        for (x, y, diameter) in self.circles:
+        for (x, y, diameter, color) in self.circles:
+            painter.setBrush(color)
             painter.drawEllipse(x, y, diameter, diameter)
 
 
